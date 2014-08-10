f0bc8d84a3bc2137442c28df8ec7672ef091a876
FabioVergani <fabio.vergani.1975@gmail.com>
2014-08-10 10:58:58 +0200

    Update defineAccessor.js

--- defineAccessor.js
+++ defineAccessor.js
@@ -14,13 +14,8 @@ function isUndefined(e){return typeof(e)==='undefined'};
 //
 function valOr(a,b){return isUndefined(a)?b:a};
 //
-function definePropertyAccessor(o,p,g,s){
- var t=String(p),n=null;
- return _defineProperty(o,p,{get:g?function(){return g(t,o,p)}:n,set:s?function(v){p=s(t,o,p,v)}:n})};
-
+function definePropertyAccessor(o,p,g,s){var t=String(p),n=null;return _defineProperty(o,p,{get:g?function(){return g(t,o,p)}:n,set:s?function(v){p=s(t,o,p,v)}:n})};
 //
-//
-/*v⇢nextvalue, p⇢actualvalue, o[t]⇢assign*/
 
 
 
@@ -34,17 +29,26 @@ function logAccess(s,w,f,o,t,p,v,x,y){
 
 //function handleBeforeSet(x){return Number(x)};
 //var f=handleBeforeSet,z=o[p],x=f(z),y=valOr(x,p);logAccess('set','assignement',f,o,t,p,v,x,y);
-//function handleBeforeGet(x){return String(x)};
+//
 //var f=handleBeforeGet,x=f(x),y=valOr(x,p);logAccess('get','returning',f,o,t,p,null,x,y);
 //TestInit:
 function Metodo(){
  var e=this,z=null;
  e.history=[];
 //function log(t,o,p,v){console.log(t+'Property:"%s" in object:%O; value new:%O, old:%O;',p,o,v)};
- function log(x,t,o,p,v){console.log('action:%s t:"%s", o:%O, p:%O, v:%O;',x,t,o,p,v)};
-  
- function Setter(t,o,p,v){log('set',t,o,p,v);p=v;return v};
- function Getter(t,o,p){log('get',t,o,p,null);v=p;return v};
+ function log(a,t,o,p,v,x){console.log('action:%s t:"%s", o:%O, p:%O, v:%O, x:%O;',a,t,o,p,v,x)};
+ 
+ function Setter(t,o,p,v){
+	var x=v+1;e.history.push(x);//⇢handleBeforeSet!
+	log('set',t,o,p,v,x);
+	return x
+ };
+ function Getter(t,o,p){
+	var x=p+2;//⇢handleAfterGet!
+	log('get',t,o,p,null,x);
+	return x
+ };
+
  definePropertyAccessor(e,'z',Getter,Setter).gethistory=function(){return e.history};
 };
 
